02231969678fb675bca1061d44fcaaf4da09577e
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-22 20:25:37 +0300

    Get away from master/slave jargon

--- GUI.py
+++ GUI.py
@@ -4,6 +4,6 @@ root.title("Just A Program")
 root.geometry("300x300+200+200")
 def btnexit():
    exit(1)
-button1=tkinter.Button(master=root,text="EXIT",fg="Red",command=btnexit)
+button1=tkinter.Button(main=root,text="EXIT",fg="Red",command=btnexit)
 button1.pack()
 root.mainloop()
